ff766ee5494f08c6640d4ce2843072281ac6b63c
Taha Aykıroğlu <tahaaykiroglu99@gmail.com>
2022-05-28 20:13:29 +0300

    Completed the project

--- 02-reviews/app.js
+++ 02-reviews/app.js
@@ -1,4 +1,4 @@
-// local reviews data
+// Local Reviews Data
 const reviews = [
   {
     id: 1,
@@ -37,3 +37,55 @@ const reviews = [
       "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ",
   },
 ];
+
+// Select Items
+const img = document.getElementById("person-img");
+const author = document.getElementById("author");
+const job = document.getElementById("job");
+const info = document.getElementById("info");
+
+const prevBtn = document.querySelector(".prev-btn");
+const nextBtn = document.querySelector(".next-btn");
+const randomBtn = document.querySelector(".random-btn");
+
+// Set Starting Item
+let currentItem = 0;
+
+// Load Initial Item
+window.addEventListener("DOMContentLoaded", function () {
+  showPerson();
+})
+
+// Show Person Based On Item
+
+function showPerson() {
+  const item = reviews[currentItem];
+  img.src = item.img;
+  author.textContent = item.name;
+  job.textContent = item.job;
+  info.textContent = item.text;
+}
+
+// Show Next Person
+nextBtn.addEventListener("click", function () {
+  currentItem++;
+  if (currentItem > reviews.length - 1) {
+    currentItem = 0;
+  }
+  showPerson();
+});
+
+// Show Previous Person
+prevBtn.addEventListener("click", function () {
+  currentItem--;
+  if (currentItem < 0) {
+    currentItem = reviews.length - 1;
+  }
+  showPerson();
+});
+
+// Show Random Person
+randomBtn.addEventListener("click", function () {
+  currentItem = Math.floor(Math.random() * reviews.length);
+  showPerson();
+});
