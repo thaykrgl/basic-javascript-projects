9ddc7e6006a63b151beb508e351206e4c05e90a6
Taha Aykıroğlu <tahaaykiroglu99@gmail.com>
2022-05-30 14:12:46 +0300

    Completed the project

--- 05-questions/app.js
+++ 05-questions/app.js
@@ -1,2 +1,13 @@
-//using selectors inside the element
-// traversing the dom
+// Traversing the DOM
+const btns = document.querySelectorAll(".question-btn");
+
+btns.forEach(function (btn) {
+  btn.addEventListener("click", function (e) {
+    const articles = document.querySelectorAll(".question");
+    articles.forEach(function (article) {
+      article.classList.remove("show-text");
+    })
+    const question = e.currentTarget.parentElement.parentElement;
+    question.classList.toggle("show-text");
+  });
+});
